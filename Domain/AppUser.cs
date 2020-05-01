dd7918f68b2fa1eb08b96c81dc3bc31d08d0584f
Carrie Schmid <carriepederson7@gmail.com>
2020-05-01 10:45:52 -0700

    Add migration for AppUser model

---
+++ Domain/AppUser.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain {
+    public class AppUser : IdentityUser {
+        public string DisplayName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Interests { get; set; }
+        public string BgCheck { get; set; }
+        public string FirstAid { get; set; }
+        public string Terms { get; set; }
+
+        //other are already built into Identity
+
+        // public virtual ICollection<UserActivity> UserActivities { get; set; }
+    }
+}
