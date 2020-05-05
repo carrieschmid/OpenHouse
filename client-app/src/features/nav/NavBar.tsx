ed5833893439a45208f4b24cf8aa0a5ed38a0e87
Carrie Schmid <carriepederson7@gmail.com>
2020-05-04 18:00:45 -0700

    Add Login button to NavBar

--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,19 +1,30 @@
 import React, { useContext } from "react";
 import { Menu, Container, Button, Dropdown, Image } from "semantic-ui-react";
-// import { observer } from "mobx-react-lite";
-// import { Link, NavLink } from "react-router-dom";
-// import { RootStoreContext } from "../../app/stores/rootStore";
-
-// import React from "./node_modules/react";
-// import { Menu, Container, Button } from "./node_modules/semantic-ui-react";
-// import { observer } from "./node_modules/mobx-react-lite";
-// import { Link } from "./node_modules/react-router-dom";
+import { observer } from "mobx-react-lite";
+import { Link, NavLink } from "react-router-dom";
+import { RootStoreContext } from "../../app/stores/rootStore";
 
 const NavBar = () => {
-  //   const rootStore = useContext(RootStoreContext);
-  //   const { user, logout } = rootStore.userStore;
+  const rootStore = useContext(RootStoreContext);
+  const { user, logout } = rootStore.userStore;
   return (
     <Menu vertical>
+      {user && (
+        <Menu.Item position="right">
+          <Image avatar spaced="right" src={user.image || "/assets/user.png"} />
+          <Dropdown pointing="top left" text={user.displayName}>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                as={Link}
+                to={`/profile/username`}
+                text="My profile"
+                icon="user"
+              />
+              <Dropdown.Item onClick={logout} text="Logout" icon="power" />
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Item>
+      )}
       <Container>
         <Menu.Item header>
           <img
