c29f4a58be1858eb2490abddce14f34908a0b654
Carrie Schmid <carriepederson7@gmail.com>
2020-06-03 16:28:20 -0700

    Add InfiniteScroll

--- client-app/src/features/sessions/dashboard/SessionDashboard.tsx
+++ client-app/src/features/sessions/dashboard/SessionDashboard.tsx
@@ -1,24 +1,47 @@
-import React, { useEffect, useContext } from "react";
-import { Grid, Button } from "semantic-ui-react";
+import React, { useEffect, useContext, useState } from "react";
+import { Grid, Button, Loader } from "semantic-ui-react";
 import SessionList from "./SessionList";
 import { observer } from "mobx-react-lite";
 import { Link, NavLink } from "react-router-dom";
 import { LoadingComponent } from "../../../app/layout/LoadingComponent";
 import { RootStoreContext } from "../../../app/stores/rootStore";
+import InfiniteScroll from "react-infinite-scroller";
+import SessionFilters from "./SessionFilters";
 
 export const SessionDashboard: React.FC = () => {
   const rootStore = useContext(RootStoreContext);
-  const { loadSessions, loadingInitial } = rootStore.sessionStore;
+  const {
+    loadSessions,
+    loadingInitial,
+    setPage,
+    page,
+    totalPages
+  } = rootStore.sessionStore;
+  const [loadingNext, setLoadingNext] = useState(false);
+
+  const handleGetNext = () => {
+    setLoadingNext(true);
+    setPage(page + 1);
+    loadSessions().then(() => setLoadingNext(false));
+  };
 
   useEffect(() => {
     loadSessions();
   }, [loadSessions]);
 
-  if (loadingInitial) return <LoadingComponent content="Loading activities." />;
+  if (loadingInitial && page === 0)
+    return <LoadingComponent content="Loading sessions...." />;
   return (
     <Grid>
       <Grid.Column width={10}>
-        <SessionList />
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={handleGetNext}
+          hasMore={!loadingNext && page + 1 < totalPages}
+          initialLoad={false}
+        >
+          <SessionList />
+        </InfiniteScroll>
       </Grid.Column>
       <Grid.Column width={6}>
         <Button
@@ -27,6 +50,11 @@ export const SessionDashboard: React.FC = () => {
           positive
           content="Create Session"
         />
+        <SessionFilters />
+      </Grid.Column>
+
+      <Grid.Column width={10}>
+        <Loader active={loadingNext} />
       </Grid.Column>
     </Grid>
   );
